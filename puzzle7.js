39d344ad3864889e5ef5575d4d550e86735e1060
kafshar <kianaafshar@berkeley.edu>
2018-01-23 02:47:26 -0800

    Create puzzle7.js

---
+++ puzzle7.js
@@ -0,0 +1,14 @@
+var red = getColor();
+function moveRudy() {
+  while (getColor() != "red") 
+   getColor(); 
+   down(); 
+}
+right(); 
+right(); 
+
+while (getColor() != "red") {
+  getColor(); 
+  down(); 
+} 
+right();
