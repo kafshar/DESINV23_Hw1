a3ded43d991d2a00b0e8cb9ab6d21ac356827578
kafshar <kianaafshar@berkeley.edu>
2018-01-23 11:32:32 -0800

    Create puzzle8.js

---
+++ puzzle8.js
@@ -0,0 +1,45 @@
+function DownEight {
+  down(); 
+  down(); 
+  down();
+  down(); 
+  down(); 
+  down();   
+  down(); 
+  down();
+}
+
+function UpSeven {
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+}
+function UpEight {
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+  up();
+}
+DownEight();
+right();
+right();
+UpSeven();
+right();
+right();
+DownEight();
+right();
+right();
+UpEight(); 
+right();
+right();
+DownEight();
+right();
+up();
