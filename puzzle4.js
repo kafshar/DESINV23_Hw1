f2c3d18990ab2943d80e53c7e2eb4b1f18155352
kafshar <kianaafshar@berkeley.edu>
2018-01-23 00:17:54 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,19 @@
+function DownFourTimes {
+  down (); 
+  down ();
+  down (); 
+  down ();
+}
+
+DownFourTimes
+right ();
+var firstColorSeen = getColor ();
+down ();
+setColor(firstColorSeen)
+right (); 
+right (); 
+right ();
+up (); 
+var secondColorSeen = getColor
+down (); 
+var setColor(secondColorSeen); 
